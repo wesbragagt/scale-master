114f470319f3abf0773718505a98fd1b0e1ef7d5
Weslley Braga <wesbragagt@gmail.com>
2020-06-07 19:30:47 -0500

    added initial setup for scale master project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Select from 'react-select'
+import scales from './scales.json'
 
-function App() {
+//  ADD WAY TO CAPITALIZE EACH LETTER SEPARATED BY -
+//  ADD SCALE DIAGRAMS FOR THE CAGED SYSTEM
+//  ADD A CHORDS THAT EACH SCALE MIGHT WORK
+
+const options = Object.entries(scales).map(scale => {
+  const [label, value] = scale
+  return { label, value }
+})
+
+function App () {
+  const [state, setState] = React.useState({ label: '', value: [] })
+
+  function onChangeHandler (selectedScale) {
+    setState(selectedScale)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Scale master</h1>
+      <h2>Type the scale you are looking for</h2>
+      <Select options={options} onChange={onChangeHandler} />
+      <main>
+        <h2>Result: </h2>
+        {state.label ? (
+          <div>
+            <h3>{state.label}</h3>
+            <div>{state.value.join(' ')}</div>
+          </div>
+        ) : (
+          <span>scale not found</span>
+        )}
+        <h2>Diagrams: </h2>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
